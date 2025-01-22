bd8b6e7578817f4b619c5546358377cf1b486560
Rahul Singh <rahulsin@Rahuls-MacBook-Pro.local>
2025-01-22 23:58:26 +0530

    Adding configuration for dvc

--- src/hyperparametertuning.py
+++ src/hyperparametertuning.py
@@ -1,13 +1,25 @@
 # hyperparameter_tuning.py
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+import pandas as pd
 import joblib
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+import numpy as np
 
 # Load dataset
-iris = load_iris()
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+data = pd.read_csv('data/car_evaluation.csv', header=None);
+data[0].replace(['vhigh', 'high','med','low'], [3, 2, 1, 0], inplace=True)
+data[1].replace(['vhigh', 'high','med','low'], [3, 2, 1, 0], inplace=True)
+data[2].replace(['5more'], [5], inplace=True)
+data[3].replace(['more'], [1], inplace=True)
+data[4].replace(['big','med','small'], [2,1,0], inplace=True)
+data[5].replace(['high','med','low'], [2,1,0], inplace=True)
+data[6].replace(['vgood','good','acc','unacc'], [3,2,1,0], inplace=True)
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Define the model
 model = RandomForestClassifier(random_state=42)
@@ -26,8 +38,29 @@ grid_search.fit(X_train, y_train)
 # Best parameters and score
 print(f"Best parameters: {grid_search.best_params_}")
 print(f"Best score: {grid_search.best_score_:.2f}")
+best_model = grid_search.best_estimator_
+
+# Evaluate the model
+y_pred = best_model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Model accuracy: {accuracy:.2f}")
+
+r2 = r2_score(y_test, y_pred)
+print(f"R² Score: {r2:.2f}")
+
+# Calculate Mean Absolute Error (MAE)
+mae = mean_absolute_error(y_test, y_pred)
+print(f"Mean Absolute Error: {mae:.2f}")
+
+# Calculate Mean Squared Error (MSE)
+mse = mean_squared_error(y_test, y_pred)
+print(f"Mean Squared Error: {mse:.2f}")
+
+# Calculate Root Mean Squared Error (RMSE)
+rmse = np.sqrt(mse)
+print(f"Root Mean Squared Error: {rmse:.2f}")
 
 # Save the best model
-best_model = grid_search.best_estimator_
+
 joblib.dump(best_model, 'model/best_model.joblib')
 print("Best model saved as 'best_model.joblib'")
--- src/train.py
+++ src/train.py
@@ -1,17 +1,23 @@
 # train_model.py
 import pandas as pd
-from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import joblib
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+import numpy as np
 
 # Load dataset
-iris = load_iris()
-print(iris)
-X = pd.DataFrame(iris.data, columns=iris.feature_names)
-y = pd.Series(iris.target)
-
+data = pd.read_csv('data/car_evaluation.csv', header=None);
+data[0].replace(['vhigh', 'high','med','low'], [3, 2, 1, 0], inplace=True)
+data[1].replace(['vhigh', 'high','med','low'], [3, 2, 1, 0], inplace=True)
+data[2].replace(['5more'], [5], inplace=True)
+data[3].replace(['more'], [1], inplace=True)
+data[4].replace(['big','med','small'], [2,1,0], inplace=True)
+data[5].replace(['high','med','low'], [2,1,0], inplace=True)
+data[6].replace(['vgood','good','acc','unacc'], [3,2,1,0], inplace=True)
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
 # Split dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -23,6 +29,21 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Model accuracy: {accuracy:.2f}")
+
+r2 = r2_score(y_test, y_pred)
+print(f"R² Score: {r2:.2f}")
+
+# Calculate Mean Absolute Error (MAE)
+mae = mean_absolute_error(y_test, y_pred)
+print(f"Mean Absolute Error: {mae:.2f}")
+
+# Calculate Mean Squared Error (MSE)
+mse = mean_squared_error(y_test, y_pred)
+print(f"Mean Squared Error: {mse:.2f}")
+
+# Calculate Root Mean Squared Error (RMSE)
+rmse = np.sqrt(mse)
+print(f"Root Mean Squared Error: {rmse:.2f}")
 
 # Save the trained model
 joblib.dump(model, 'model/model.joblib')
